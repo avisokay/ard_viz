b8eab1c34b021cd4ab78f9ab6e37cd51c968a10a
Adam Visokay <anv3ab@virginia.edu>
2023-02-08 15:28:55 -0800

    adding viz.R file to remote repo from local

---
+++ viz.R
@@ -0,0 +1,6 @@
+# https://cengel.github.io/R-data-viz/domains.html#networks-e.g.-visnetwork
+# https://holtzy.github.io/Pimp-my-rmd/
+# https://bookdown.org/yihui/rmarkdown-cookbook/diagrams.html
+
+
+
